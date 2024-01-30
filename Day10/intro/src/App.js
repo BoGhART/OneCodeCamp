7f906150a4ad42829dd94aaf70f81abca9667e33
BoGhART <bogartbogzz21@gmail.com>
2024-01-30 16:17:38 +0800

    Day10

---
+++ Day10/intro/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Welcome from './component/Welcome';
+import './App.css';
+// import Frog from './assets/imgs/frog.jpg'
+// import Images from './component/Images';
+// import Child from './component/Child';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       {/* <Images/> */}
+
+//       <Child name="Dion"/>
+
+//     </div>
+//   );
+// }
+
+function App() {
+  return (
+    <div>
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
+
+//JSX - js extension
---
+++ Day10/intro/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// const trainee = "Bogart"
+// const element = <h1>Hello, {trainee}</h1>
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
